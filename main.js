507f75d3b1e6c0de39770af9984f9e41671e6347
Augustin Bégué <augustin.begue@epita.fr>
2021-09-14 23:08:17 +0200

    init maggle

--- main.js
+++
@@ -1,9 +0,0 @@
-import { CharacterWarp } from "./character-warp-speed.js";
-
-window.onload = () => {
-    let el = document.getElementById("characterWarp");
-
-    if (el) {
-        new CharacterWarp(el, ["1", "2", "3", "4", "5", "6", "7", "8", "9"]);
-    }
-};
---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+    import CharacterWarpSpeed from "character-warp-speed";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        let el = document.getElementById("characterWarpSpeed");
+        let cws = new CharacterWarpSpeed(el, [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "0",
+        ]);
+    });
+</script>
+
+<main>
+    <h1>✨ Character Warp Speed ✨</h1>
+
+    <section>
+        <div id="characterWarpSpeed" />
+    </section>
+</main>
+
+<style>
+    section {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #characterWarpSpeed {
+        width: 90vw;
+        height: 90vh;
+        overflow: hidden;
+    }
+
+    main {
+        text-align: center;
+        margin: 0;
+        overflow: hidden;
+    }
+
+    h1 {
+        color: rgb(37, 37, 37);
+        text-transform: uppercase;
+        font-size: 3em;
+        font-weight: 700;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
